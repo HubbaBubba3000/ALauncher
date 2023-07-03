a4d73fb2b4afe249a63e501560fdb3f56b11c710
HubbaBubba3000 <100069933+HubbaBubba3000@users.noreply.github.com>
2023-07-03 19:57:42 +0300

    7.3.23 fix style

--- Data/Folder.cs
+++ Data/Folder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace ALauncher.Data
 {
     public class Folder {
-        public string Name { get; set; }
-        public List<Item> Items { get; set; }
+        public string? Name { get; set; }
+        public List<Item>? Items { get; set; }
     }
 }
--- Data/Item.cs
+++ Data/Item.cs
@@ -1,9 +1,9 @@
 namespace ALauncher.Data
 {
     public class Item {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string File { get; set; }
+        public string? Name { get; set; }
+        public string? Path { get; set; }
+        public string? File { get; set; }
 
     }
 }
